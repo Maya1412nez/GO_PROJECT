26c2436fa1a7a1785ecc4fbcc6e43d7df8902a4f
VladimirLisenkov <114505544+VladimirLisenkov@users.noreply.github.com>
2024-11-30 03:48:23 +0300

    Update back.go

--- initBack/back.go
+++ initBack/back.go
@@ -2,36 +2,24 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
-
-type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
-}
-
-func (u *User) getAllInfo() string {
-	return fmt.Sprintf("usernameis:%s. heis %d and he has money %d", u.name, u.age, u.money)
-}
-
-func (u *User) setNewName(newName string) {
-	u.name = newName
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	bob := User{"BOB", 25, -50, 4.2, 0.8}
-	bob.setNewName("alex")
-	fmt.Fprintf(w, bob.getAllInfo())
-}
 
 func contactsPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ContactsPage")
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/main.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "main.html", nil)
+}
+
 func HandleRequest() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", index)
 	http.HandleFunc("/contacts/", contactsPage)
 	http.ListenAndServe(":8080", nil)
 }
